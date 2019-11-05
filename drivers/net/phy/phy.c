7f4bb3bf2103578c516047a28dc0d40da7d99d6c
Russell King <rmk+kernel@armlinux.org.uk>
2019-11-05 10:34:39 +0000

    net: phy: make phy_error() report which PHY has failed
    phy_error() is called from phy_interrupt() or phy_state_machine(), and
    uses WARN_ON() to print a backtrace. The backtrace is not useful when
    reporting a PHY error.
    
    However, a system may contain multiple ethernet PHYs, and phy_error()
    gives no clue which one caused the problem.
    
    Replace WARN_ON() with a call to phydev_err() so that we can see which
    PHY had an error, and also inform the user that we are halting the PHY.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -912,7 +912,7 @@ void phy_stop_machine(struct phy_device *phydev)
  */
 void phy_error(struct phy_device *phydev)
 {
-	WARN_ON(1);
+	phydev_err(phydev, "Error detected, halting PHY\n");
 
 	mutex_lock(&phydev->lock);
 	phydev->state = PHY_HALTED;
