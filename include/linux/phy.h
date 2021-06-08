cb0eca285bbbf23fc380dfec52d2110cd31b4598
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2021-06-08 14:43:54 +0100

    net: phy: add phy_interface_t bitmap support
    Add support for a bitmap for phy interface modes, which includes:
    - a macro to declare the interface bitmap
    - an inline helper to zero the interface bitmap
    - an inline helper to detect an empty interface bitmap
    - an inline helper to do a bitwise AND operation on two interface
      bitmaps
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -155,6 +155,26 @@ typedef enum {
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
+/* PHY interface mode bitmap handling */
+#define DECLARE_PHY_INTERFACE_MASK(name) \
+	DECLARE_BITMAP(name, PHY_INTERFACE_MODE_MAX)
+
+static inline void phy_interface_zero(unsigned long *intf)
+{
+	bitmap_zero(intf, PHY_INTERFACE_MODE_MAX);
+}
+
+static inline bool phy_interface_empty(const unsigned long *intf)
+{
+	return bitmap_empty(intf, PHY_INTERFACE_MODE_MAX);
+}
+
+static inline void phy_interface_and(unsigned long *dst, const unsigned long *a,
+				     const unsigned long *b)
+{
+	bitmap_and(dst, a, b, PHY_INTERFACE_MODE_MAX);
+}
+
 /*
  * phy_supported_speeds - return all speeds currently supported by a PHY device
  */
