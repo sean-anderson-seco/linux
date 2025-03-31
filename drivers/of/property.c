567f80e6974f1d0d16d84ef33a4b219a51f7e61c
Sean Anderson <sean.anderson@linux.dev>
2025-03-31 11:38:44 -0400

    of: property: Add device link support for PCS
    This adds device link support for PCS devices. Both the recommended
    pcs-handle and the deprecated pcsphy-handle properties are supported.
    This should provide better probe ordering.
    
    Signed-off-by: Sean Anderson <sean.anderson@linux.dev>
    Patch-cc: Saravana Kannan <saravanak@google.com>
    Patch-cc: Frank Rowand <frowand.list@gmail.com>
    Patch-cc: Rob Herring <robh+dt@kernel.org>
    Patch-cc: devicetree@vger.kernel.org

--- drivers/of/property.c
+++ drivers/of/property.c
@@ -1379,6 +1379,8 @@ DEFINE_SIMPLE_PROP(pses, "pses", "#pse-cells")
 DEFINE_SIMPLE_PROP(power_supplies, "power-supplies", NULL)
 DEFINE_SUFFIX_PROP(regulators, "-supply", NULL)
 DEFINE_SUFFIX_PROP(gpio, "-gpio", "#gpio-cells")
+DEFINE_SIMPLE_PROP(pcs_handle, "pcs-handle", NULL)
+DEFINE_SIMPLE_PROP(pcsphy_handle, "pcsphy-handle", NULL)
 
 static struct device_node *parse_gpios(struct device_node *np,
 				       const char *prop_name, int index)
@@ -1535,6 +1537,8 @@ static const struct supplier_bindings of_supplier_bindings[] = {
 		.parse_prop = parse_post_init_providers,
 		.fwlink_flags = FWLINK_FLAG_IGNORE,
 	},
+	{ .parse_prop = parse_pcs_handle, },
+	{ .parse_prop = parse_pcsphy_handle, },
 	{}
 };
 
